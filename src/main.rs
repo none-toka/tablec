b09d2a51a589a1d9beb8ba0b2e1385f14749e3f7
naokata <snaokata@gmail.com>
2020-01-21 21:02:53 +0900

    add parser for arguments

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::error::Error;
 use std::io;
 use std::process;
 
+use clap::{App, Arg, SubCommand};
+
 fn example() -> Result<(), Box<dyn Error>> {
     // Build the CSV reader and iterate over each record.
     let mut rdr = csv::Reader::from_reader(io::stdin());
@@ -15,6 +17,41 @@ fn example() -> Result<(), Box<dyn Error>> {
 }
 
 fn main() {
+    let app = App::new("tablec")
+        .version("0.1.0") // バージョン情報
+        .author("SHIKUMA Naokata <snaokata@gmail.com>") // 作者情報
+        .about("Tablec is a table converter tool.") // このアプリについて
+        .arg(
+            Arg::with_name("pa") // 位置引数を定義
+                .help("sample positional argument") // ヘルプメッセージ
+                .required(true), // この引数は必須であることを定義
+        )
+        .arg(
+            Arg::with_name("flg") // フラグを定義
+                .help("sample flag") // ヘルプメッセージ
+                .short("f") // ショートコマンド
+                .long("flag"), // ロングコマンド
+        )
+        .arg(
+            Arg::with_name("opt") // オプションを定義
+                .help("sample option") // ヘルプメッセージ
+                .short("o") // ショートコマンド
+                .long("opt") // ロングコマンド
+                .takes_value(true), // 値を持つことを定義
+        )
+        .subcommand(
+            SubCommand::with_name("sub") // サブコマンドを定義
+                .about("sample subcommand") // このサブコマンドについて
+                .arg(
+                    Arg::with_name("subflg") // フラグを定義
+                        .help("sample flag by sub") // ヘルプメッセージ
+                        .short("f") // ショートコマンド
+                        .long("flag"), // ロングコマンド
+                ),
+        );
+
+    let matches = app.get_matches();
+    println!("{:?}", matches);
     if let Err(err) = example() {
         println!("error running example: {}", err);
         process::exit(1);
